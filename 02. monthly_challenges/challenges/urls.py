b6ce3929eef8fd05df1a824cb27394c29dd696e3
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-04-20 02:22:49 +0000

    Practicing URLs, Views and Dynamic View Logic

--- 02. monthly_challenges/challenges/urls.py
+++ 02. monthly_challenges/challenges/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path("january/", views.january),
-    # path("february/", views.february),
-    # path("march/", views.march),
+    path("", views.index),
     path("<int:month>/", views.monthly_challenges_by_numbers),
     path("<str:month>/", views.monthly_challenge, name="month-challenge")
 ]

5757a86d70d301632c1a3ec1a673b69280a48aa1
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-04-20 00:58:28 +0000

    The reverse function in python django

--- 02. monthly_challenges/challenges/urls.py
+++ 02. monthly_challenges/challenges/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # path("february/", views.february),
     # path("march/", views.march),
     path("<int:month>/", views.monthly_challenges_by_numbers),
-    path("<str:month>/", views.monthly_challenge)
+    path("<str:month>/", views.monthly_challenge, name="month-challenge")
 ]

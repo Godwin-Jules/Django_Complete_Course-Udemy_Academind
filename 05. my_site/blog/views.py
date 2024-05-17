ea7f9fb6017b2bebb7ec9721b41bff91b0553a0a
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-17 11:30:48 +0000

    Setting up my_site project with blog as an app (feature)

---
+++ 05. my_site/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, '404.html', {})
+    # return render(request, 'blog/index.html', {})
+
+def posts(request):
+    return render(request, '404.html', {})
+    # return render(request, 'blog/posts', {})
+
+def post(request):
+    return render(request, '404.html', {})
+    # return render(request, 'blog/post', {})

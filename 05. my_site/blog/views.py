54b098c8cc6f9ca5327e457359a60d2cbe3470ef
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-24 15:36:25 +0000

    Adding all-posts page, CSS files and linking the index and the all-posts pages

--- 05. my_site/blog/views.py
+++ 05. my_site/blog/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'blog/index.html', {})
 
 def posts(request):
-    return render(request, 'blog/posts', {})
+    return render(request, 'blog/all-posts.html', {})
 
 def post(request, slug):
     return render(request, 'blog/post', {})

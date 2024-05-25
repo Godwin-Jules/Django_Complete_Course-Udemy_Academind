04f4f6b39bc54e4cbc8a6b689f0b50f3db3c0fce
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-25 22:49:09 +0000

    Querying & Filtering data

--- 06. book_store/book_outlet/models.py
+++ 06. book_store/book_outlet/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+books = [
+    {"Harry Potter 1 - The Philosopher's Stone", "J.K. Rowling", 5, True},
+    {"Lord of the Rings", "J.R.R. Tolkien", 4, True}
+]
+"""For example:
+    * Book.objects.get() return a single object so make sure that the parameter or criteria you put in get method must retrieve only one or no object
+    * Book.objects.filter() return a list of objects according to the parameter you put into it. Never write : Book.objects.filter(rating<5), ... but there is another syntax to do that
+    * Book.objects.filter(rating__lt = 4)
+        - __lt          =>  lower than
+        - __lte         =>  lower than or equal
+        - __gt          =>  greater than
+        - __gte         =>  greater than or equal
+        - __exact       =>  == (case sensitive)
+        - __iexact      =>  ILIKE '%value%' (not case sensitive)
+        - __in          =>  IN in SQL query statement
+        - __contains    =>  LIKE '%value%' (case sensitive)
+        - __icontains   =>  LIKE '%value%' (not case sensitive)
+        - __startswith  =>  LIKE 'value%' (case sensitive)
+        - __istartwith  =>  LIKE 'value%' (not case sensitive)
+        - __endwith     =>  LIKE '%value' (vase sensitive)
+        - __iendwith    =>  LIKE '%value' (not case sensitive)
+        - __range       =>  BETWEEN in SQL query statement
+"""
+
 # Create your models here.
 
 class Book(models.Model):

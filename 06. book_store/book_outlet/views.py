c2dd406b56fb923ac5b318d6cecb75f95f264699
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-27 17:09:18 +0000

    Preparing and Rendering Templates

--- 06. book_store/book_outlet/views.py
+++ 06. book_store/book_outlet/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
+from .models import Book as b
 # Create your views here.
+
+def index(request):
+    books = b.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'books': books
+    })
+
+def book_detail(request, book_id):
+    # try:
+    #     book = b.objects.get(pk = book_id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(b, pk = book_id)
+    return render(request, 'book_outlet/book_detail.html', {
+        'book': book
+    })

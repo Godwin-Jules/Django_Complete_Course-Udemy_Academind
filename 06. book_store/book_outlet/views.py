14ce4c6d8b50404d78af80b3337f3cfa8e8fb3ca
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-27 22:15:45 +0000

    Aggregation & ordering

--- 06. book_store/book_outlet/views.py
+++ 06. book_store/book_outlet/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book as b
 # Create your views here.
 
 def index(request):
-    books = b.objects.all()
+    books = b.objects.all().order_by('-rating')   # By default, the order is 'ascending', to make it 'descending', you just need to add a minus sign before the column name like b.objects.all().order_by('-title')
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, 'book_outlet/index.html', {
-        'books': books
+        'books': books,
+        'total_number_of_books': num_books,
+        'average_rating': avg_rating
     })
 
 def book_detail(request, book_slug):

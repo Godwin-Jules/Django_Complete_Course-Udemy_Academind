fdee1b044daae5843e051251633fa86baaca2e67
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-30 15:05:56 +0000

    Creating, Configuring and Using the DataBase and Configuring the Admin Panel for the Blog project

--- 05. my_site/blog/admin.py
+++ 05. my_site/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email_address',)
+    list_filter = ('first_name', 'last_name',)
+    sortable_by = ('first_name', 'last_name',)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'date', 'author', 'slug',)
+    list_filter = ('title', 'date',)
+
+admin.site.register(Tag)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)

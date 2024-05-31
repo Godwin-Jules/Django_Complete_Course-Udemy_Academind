9cfd697c35155fd3d845e1ba61fb193d3fdd10f9
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-31 14:27:57 +0000

    Manual form validation & its problem

--- 12. feedback/reviews/views.py
+++ 12. feedback/reviews/views.py
@@ -7,9 +7,12 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['name']
-        print(entered_username)
+        
+        if entered_username == '':
+            return render(request, 'reviews/review.html', {'has_error': True})
+        
         return HttpResponseRedirect('/thank-you/')
-    return render(request, 'reviews/review.html')
+    return render(request, 'reviews/review.html', {'has_error': False})
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')

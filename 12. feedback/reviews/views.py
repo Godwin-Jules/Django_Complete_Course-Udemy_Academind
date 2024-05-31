67baaf95a0a8d1c0e5b8332cb85de158da59aeef
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-05-31 14:17:46 +0000

    GET & POST Requests + CSRF token protection + Handling form submission & Extraction Data

--- 12. feedback/reviews/views.py
+++ 12. feedback/reviews/views.py
@@ -1,6 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+# CSRF stands for Cross Site Request Forgery
 
 def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['name']
+        print(entered_username)
+        return HttpResponseRedirect('/thank-you/')
     return render(request, 'reviews/review.html')
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')

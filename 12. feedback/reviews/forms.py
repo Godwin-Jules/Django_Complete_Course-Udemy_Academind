795651e02ffee9a69c393c9ef5da73830b76368e
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-06-06 12:29:16 +0000

    Using Django Form Class & Form Validation

---
+++ 12. feedback/reviews/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class ReviewForm(forms.Form):
+    user_name = forms.CharField(label='Your name', required=False, max_length=100, error_messages={
+        'required': 'Your name must not be empty',
+        'max_length': 'Please enter a shorter name!'
+    })
+"""
+    You can override some attributes of or behavior of a form by parsing some arguments to the field constructor
+"""

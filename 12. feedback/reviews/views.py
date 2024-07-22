da966c58c9ff7f257696c1eec075c3a4e9dba4b5
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-07-22 15:16:49 +0000

    Class Based Views & Updating Django and pip respectively from 5.0.2 to 5.0.7 and 24.0 to 24.1.2

--- 12. feedback/reviews/views.py
+++ 12. feedback/reviews/views.py
@@ -1,21 +1,24 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 # CSRF stands for Cross Site Request Forgery
 
-def review(request):
-    if request.method == 'POST':
-        existing_model = Review.objects.get(pk=1)
-        form = ReviewForm(request.POST, instance = existing_model)
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {'form': form})
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/thank-you')
-    else:
-        form = ReviewForm()
-    return render(request, 'reviews/review.html', {'form': form})
+        else:
+            return render(request, 'reviews/review.html', {'form': form})
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')

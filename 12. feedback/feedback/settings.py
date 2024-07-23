003795291de3f22b19c2dfd36446701700d70d81
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-07-23 23:17:21 +0000

    Enablling & Configuring Sessions

--- 12. feedback/feedback/settings.py
+++ 12. feedback/feedback/settings.py
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL = '/user-media/'
+
+# SESSION_COOKIE_AGE = 120 # unit in seconds. By default SESSION_COOKIE_AGE = 2 weeks
